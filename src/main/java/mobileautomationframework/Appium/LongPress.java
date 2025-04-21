ac2bb19d6a3e75d29f48cbd0398b62ab9ec2d6b7
Piyush Akoliya <piyushakoliya@TEB78.WPA.Dal.Ca>
2025-04-20 21:17:29 -0300

    LongPress and Scroll Gestures

---
+++ src/main/java/mobileautomationframework/Appium/LongPress.java
@@ -0,0 +1,61 @@
+package mobileautomationframework.Appium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+/**
+ * Hello world!
+ */
+public class LongPress extends BaseTestClass {
+
+	@Test
+	public void appiumTest() throws MalformedURLException, InterruptedException {
+
+		checkAndroidEnvVariables();
+
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
+		driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
+		WebElement element = driver.findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
+		
+		longPressItem(element);
+		
+		String menuText= driver.findElement(By.id("android:id/title")).getText();
+		Assert.assertEquals(menuText,"Sample menu");
+		Assert.assertTrue(driver.findElement(By.id("android:id/title")).isDisplayed());
+
+	}
+
+	public static void checkAndroidEnvVariables() {
+		String androidHome = System.getenv("ANDROID_HOME");
+		String androidSdkRoot = System.getenv("ANDROID_SDK_ROOT");
+
+		if (androidHome == null || androidHome.isEmpty()) {
+			System.out.println("❌ ANDROID_HOME is not set.");
+		} else {
+			System.out.println("✅ ANDROID_HOME: " + androidHome);
+		}
+
+		if (androidSdkRoot == null || androidSdkRoot.isEmpty()) {
+			System.out.println("❌ ANDROID_SDK_ROOT is not set.");
+		} else {
+			System.out.println("✅ ANDROID_SDK_ROOT: " + androidSdkRoot);
+		}
+	}
+}
---
+++ src/main/java/mobileautomationframework/Appium/ScrollDemo.java
@@ -0,0 +1,36 @@
+package mobileautomationframework.Appium;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.net.MalformedURLException;
+
+import io.appium.java_client.AppiumBy;
+
+/**
+ * Scroll Demo 
+ * Here we are using the UiScrollable class and also the Google Engine as we are using androidUIAutomator
+ */
+public class ScrollDemo extends BaseTestClass {
+
+	@Test
+	public void appiumTest() throws MalformedURLException, InterruptedException {
+
+		driver.findElement(AppiumBy.accessibilityId("Views")).click();
+		
+		//If you know where to scroll, then use this.
+//		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))"));
+		
+		// Else use the exact pixel point value to where you want to scroll
+		// Java
+//		while((Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+//		    "left", 100, "top", 100, "width", 200, "height", 200,
+//		    "direction", "down",
+//		    "percent", 1.0
+//		))){
+//			
+//		};
+	}
+}
